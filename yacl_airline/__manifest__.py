961391f6f65e7ee3df9634f64f37f9df29053027
Aung Phone <aungphone.mm@gmail.com>
2024-12-24 09:44:26 +0630

    report bug fix

--- yacl_airline/__manifest__.py
+++ yacl_airline/__manifest__.py
@@ -18,21 +18,20 @@
         'views/yacl_airline_views.xml',
         'views/yacl_flights_view.xml',
         'views/menu.xml',
+        'wizard/report_airline_charges_wizard_view.xml',
+        'report/airline_charges_report.xml',
+
+        'wizard/report_landing_wizard_view.xml',
+        'report/landing_summary_report.xml',
+
+        'wizard/checkin_report_wizard_view.xml',
+        'report/checkin_report_template.xml',
         # Invoice wizard and report
         'wizard/report_invoice_summary_wizard_view.xml',
         'report/invoice_summary_report.xml',
         # Passenger wizard and report
         'wizard/report_passenger_wizard_view.xml',
         'report/passenger_summary_report.xml',
-
-        'wizard/report_landing_wizard_view.xml',
-        'report/landing_summary_report.xml',
-
-        'wizard/report_airline_charges_wizard_view.xml',
-        'report/airline_charges_report.xml',
-
-        'wizard/checkin_report_wizard_view.xml',
-        'report/checkin_report_template.xml',
 
         'report/subscription_report.xml',
     ],
